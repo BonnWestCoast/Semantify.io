1f31e8c778e03344c4e425a3129f9922457dbd7d
cherurg <karpovad@yandex.ru>
2016-06-15 19:43:23 +0200

    Added comments for schemas selectors

--- src/redux/selectors/schemasList.js
+++ src/redux/selectors/schemasList.js
@@ -1,8 +1,10 @@
+// returns the list of schemas
 export function list(state) {
   let schemas = state.schemasList.list
   return Object.keys(schemas).map(id => ({...schemas[id], id}))
 }
 
+// returns a schema by its ID
 export function schema(state, id) {
   return state.schemasList.list[id]
 }
