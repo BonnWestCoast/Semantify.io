3d78ab7df2ffac0e4526106073d91e707c47715f
cherurg <karpovad@yandex.ru>
2016-06-15 19:39:50 +0200

    Added comments for containers/SchemaList

--- src/containers/SchemasList/SchemasList.js
+++ src/containers/SchemasList/SchemasList.js
@@ -1,9 +1,13 @@
 import React, {Component, PropTypes} from 'react'
 import {connect} from 'react-redux'
+
+// components
 import {Link} from 'react-router'
 
+// selectors
 import {list as getList} from 'redux/selectors/schemasList'
 
+// this class shows the list of all previously uploaded files
 @connect(state => ({list: getList(state)}))
 export default class SchemasList extends Component {
   static propTypes = {
@@ -15,7 +19,13 @@ export default class SchemasList extends Component {
       <div className="container">
         <h1>Schemas List</h1>
         <ul>
-          {this.props.list.map(it => <li key={it.id}><Link to={`/schema/${it.id}`}>{it.name}</Link></li>)}
+          {
+            this.props.list.map(it =>
+              <li key={it.id}>
+                <Link to={`/schema/${it.id}`}>{it.name}</Link>
+              </li>
+            )
+          }
         </ul>
       </div>
     )
