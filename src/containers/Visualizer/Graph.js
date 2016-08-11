7b506fd90b85dd1dbcf8058c9d7c8c3a1abb98ac
aw3s0me <aw3s0me@rocketmail.com>
2016-08-11 23:11:29 +0200

    Now can expand nodes

--- src/containers/Visualizer/Graph.js
+++ src/containers/Visualizer/Graph.js
@@ -39,10 +39,60 @@ export default class Graph extends Component {
   }
 
   bindEvents() {
-    this.chart.on("doubleClick", function (params) {
-      params.event = "[original event]";
+    this.chart.on('doubleClick', (params) => {
+      params.event = '[original event]';
+      let selNodes = params.nodes;
+      for (let node of selNodes) {
+        this.expandNode(node);
+      }
       console.log('<h2>doubleClick event:</h2>' + JSON.stringify(params, null, 4));
     });
+  }
+
+  /**
+   * Method to expand node
+   * @param node
+     */
+  expandNode(node) {
+    // connections to node (arr of ids)
+    let connections = this.props.graph.toFromLookup[node];
+    if (!connections) {
+      return;
+    }
+
+    let allEdges = this.props.graph.edges;
+    let nodesToAdd = [];
+    let edgesToAdd = [];
+
+    for (let connection of connections) {
+      // create node
+      nodesToAdd.push({
+        id: connection,
+        label: connection
+      });
+    }
+    // first update nodes, otherwise we cannot add edges
+    // which are connected to new nodes
+    this.nodes.update(nodesToAdd);
+
+    for (let connection of connections) {
+      let connectionEdges = allEdges[connection];
+      for (let edge of connectionEdges) {
+        if (this.isValidEdge(edge)) {
+          edgesToAdd.push(edge);
+        }
+      }
+    }
+
+    this.edges.update(edgesToAdd);
+  }
+
+  /**
+   * Check if edge really has connection with node
+   * @param edge
+     */
+  isValidEdge(edge) {
+    return edge && this.nodes.get(edge.to);
   }
 
   /**
