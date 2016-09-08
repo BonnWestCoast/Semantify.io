d06777cc0e5bef2d6d074ef83f850291d5b0500e
cherurg <karpovad@yandex.ru>
2016-09-08 22:19:06 +0300

    Finished ActivableLink component

--- src/components/ActivableLink/ActivableLink.js
+++ src/components/ActivableLink/ActivableLink.js
@@ -1,15 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
+
+let defaultStyle = {}
+
+let defaultActiveStyle = { // this style is applied when the link is active
+  fontWeight: 'bold'
+}
 
 /**
  * ActivableLink component is needed to display active links
  */
 export default class ActivableLink extends Component {
+  static propTypes = {
+    to: PropTypes.string,
+    style: PropTypes.object,
+    activeStyle: PropTypes.object
+  }
+
+  static contextTypes = {
+    router: React.PropTypes.object // injecting react-router
+  }
+
   render() {
-    let {...rest } = this.props
+    let style = Object.assign({}, defaultStyle, this.props.style)
+
+    let { router } = this.context
+    if (router && router.isActive(this.props.to)) { // the link is active
+      style = Object.assign(style, defaultActiveStyle, this.props.activeStyle) // apply the active
+      // style
+    }
 
     return (
-      <Link {...rest}/>
+      <Link {...this.props} style={style}/>
     )
   }
 }
